8378a720115e9dbce5212e578a3ee5ceea9af49a
Deco Moraes <decomoraes@MacBook-Pro-de-Deco.local>
2020-05-19 16:01:15 -0300

    Updates

---
+++ precache-manifest.133c0a9efa5c559629534a0bff69273f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67709a2c310289808ff2e80410e1e85d",
+    "url": "/decomoraes.github.io/index.html"
+  },
+  {
+    "revision": "fc852331cfdc04e0f72a",
+    "url": "/decomoraes.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5fc8c894f08b134872a",
+    "url": "/decomoraes.github.io/static/js/2.21d07ca0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/decomoraes.github.io/static/js/2.21d07ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc852331cfdc04e0f72a",
+    "url": "/decomoraes.github.io/static/js/main.5551d1f1.chunk.js"
+  },
+  {
+    "revision": "18a4b0ed9c72fb8dda5c",
+    "url": "/decomoraes.github.io/static/js/runtime-main.4df8a70a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/decomoraes.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
